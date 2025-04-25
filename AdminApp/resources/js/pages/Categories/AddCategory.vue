8892fbd6caa4813cbb08ae488bdff8747055eecd
Ali <mohammedakbarali96@gmail.com>
2025-04-25 08:29:32 +0400

    Slider added with SliderList.vue, AddSlider.vue, EditSlider.vue

---
+++ AdminApp/resources/js/pages/Categories/AddCategory.vue
@@ -0,0 +1,88 @@
+<script setup>
+import { Link, useForm, usePage } from "@inertiajs/vue3";
+import Layout from "../shared/Layout.vue";
+import { computed, onMounted, ref } from "vue";
+import { useToast } from "vue-toastification";
+
+const flash = computed(() => usePage().props.flash);
+
+const toast = useToast();
+
+const form = useForm({
+    name: "",
+    image: null,
+});
+
+const categoryImage = ref();
+
+const handleImage = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+        form.image = file;
+        categoryImage.value = URL.createObjectURL(file);
+    }
+};
+const submitForm = () => {
+    form.post('/categories', {
+        onSuccess: () => {
+            flash.value.success && toast.success(flash.value.success)
+            flash.value.error && toast.error(flash.value.error)
+        }
+    })
+};
+</script>
+<template>
+    <Layout>
+        <main class="ml-64 p-8">
+            <h2 class="text-2xl font-bold mb-6">Add Category</h2>
+            <form
+                class="bg-white shadow-md rounded-lg p-6"
+                @submit.prevent="submitForm"
+            >
+                <div class="mb-4">
+                    <label
+                        for="category-name"
+                        class="block text-gray-700 font-bold mb-2"
+                        >Name</label
+                    >
+                    <input
+                        type="text"
+                        id="category-name"
+                        class="w-full p-2 border rounded-md"
+                        placeholder="Enter category name"
+                        required
+                        v-model="form.name"
+                    />
+                </div>
+                <div class="mb-4">
+                    <label
+                        for="category-image"
+                        class="block text-gray-700 font-bold mb-2"
+                        >Image</label
+                    >
+                    <input
+                        type="file"
+                        id="category-image"
+                        class="w-full p-2 border rounded-md"
+                        required
+                        v-on:change="handleImage"
+                    />
+                </div>
+                <div class="mb-4">
+                    <img
+                        :src="categoryImage"
+                        alt="Category Image"
+                        class="w-25 h-25 rounded-full md-2"
+                    />
+                </div>
+                <button
+                    type="submit"
+                    class="bg-blue-500 text-white px-4 py-2 rounded-md"
+                >
+                    Add Category
+                </button>
+            </form>
+        </main>
+    </Layout>
+</template>
+<script></script>
--- AdminApp/routes/web.php
+++ AdminApp/routes/web.php
@@ -1,12 +1,18 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BrandController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\SliderController;
 use App\Http\Controllers\SSLCommerzCredentialController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('page.dashboard');
 Route::resource('/settings', SSLCommerzCredentialController::class);
+Route::resource('/brands', BrandController::class);
+Route::resource('/categories', CategoryController::class);
+Route::resource('/sliders', SliderController::class);
 
 Route::get('/login', [AuthController::class, 'loginPage'])->name('login');
 Route::post('/login', [AuthController::class, 'login'])->name('login.post');
