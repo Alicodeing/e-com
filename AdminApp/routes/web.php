789f2850e43097b5cbe99e8efa55f6ee2cbb43e3
Ali <mohammedakbarali96@gmail.com>
2025-04-25 09:45:01 +0400

    Order page

--- AdminApp/routes/web.php
+++ AdminApp/routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\SliderController;
+use App\Http\Controllers\OrderController;
 use App\Http\Controllers\SSLCommerzCredentialController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +16,7 @@ Route::resource('/brands', BrandController::class);
 Route::resource('/categories', CategoryController::class);
 Route::resource('/sliders', SliderController::class);
 Route::resource('/products', ProductController::class);
+Route::resource('/orders', OrderController::class);
 
 Route::get('/login', [AuthController::class, 'loginPage'])->name('login');
 Route::post('/login', [AuthController::class, 'login'])->name('login.post');
